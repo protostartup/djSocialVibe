47da22d52df72fb09f0f4665296e73a7611b905f
trioka <christopherk@triokasoftware.com>
2018-09-30 15:13:02 +0530

    parallax

--- bigday/static/assets/js/common.js
+++ bigday/static/assets/js/common.js
@@ -312,7 +312,7 @@ function parallaxInit() {
 function parallax() {
     var scrollPosition = $(window).scrollTop();
     $('#parallax').css('top', (0 - (scrollPosition * 0.3)) + 'px'); // bg image moves at 30% of scrolling speed
-    $('#hero').css('opacity', ((100 - scrollPosition / 2) * 0.01));
+    // $('#hero').css('opacity', ((100 - scrollPosition / 2) * 0.01));
 }
 
 
