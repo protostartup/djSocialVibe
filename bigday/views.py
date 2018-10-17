de076119cf25e73fb080ef97c986003f38689cb0
trioka <christopherk@triokasoftware.com>
2018-10-17 12:48:26 +0530

    added send form

--- bigday/views.py
+++ bigday/views.py
@@ -18,29 +18,30 @@ def contact(request):
     if request.method == 'GET':
         form = ContactForm()
     else:
-        form = ContactForm(request.POST)
-        if form.is_valid():
-	    #messages.success(request,'Form submission successful')
-            contact_name = form.cleaned_data['username']
-            contact_email = form.cleaned_data['usermail']
-	    contact_subject = form.cleaned_data['phone']
-            content = form.cleaned_data['message']
-            try:
-                email = EmailMessage(contact_name,
-                                    content,
-                                    contact_email,
-                                    ['protostartup@gmail.com'], #change to your email
-                                     reply_to=[contact_email],
-                                   )
-                email.send()
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('/thanks/')
-    return render(request, 'contact.html', {'form': form})
+        form = ContactForm()
+        form.username=request.POST.get('name')
+        form.email=request.POST.get('email')
+        form.phone=request.POST.get('phone')
+        form.message=request.POST.get('message')
+
+        try:
+            email = EmailMessage(subject = form.username,
+                                 body =form.message+" "+form.phone,
+                                 from_email='protostartup@gmail.com', #change to your email
+                                 to=['protostartup@gmail.com'],
+                                 reply_to=[form.email],
+                                 #headers = form.phone
+                                 )
+            email.send()
+        except BadHeaderError:
+            print("err")
+            return HttpResponse('Invalid header found.')
+        return redirect('/thanks/')
+    return render(request, './donate.html', {'form': form})
 
 
 def thanks(request):
-    return render(request, './home.html', {})
+    return render(request, './index.html', {})
 
 # def donate(request):
 #     if request.method == 'GET':
