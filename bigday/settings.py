6b3a16eaa2a97ffd3b66ba5128e4bcc92239dd42
ChrizKiran <christopherkiran12@gmail.com>
2018-11-01 10:58:37 +0530

    added google forms

--- bigday/settings.py
+++ bigday/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "u7!-y4k1c6b44q507nr_l+c^12o7ur++cpzyn!$65w^!gum@h%"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
